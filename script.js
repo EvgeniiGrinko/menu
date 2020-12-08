6c728983ea8679ea5ae2213bafa4334193a740b4
Evgenii <jenek0907@mail.ru>
2020-12-08 23:45:42 +0300

    json

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menus: []
+        };
+    }
+    componentDidMount() {
+        fetch("./menus.json").then(response => response.json()).then(menus => this.setState({ menus: menus }));
+    }
     render() {
-        let menus = ['Home', 'About', 'Services', 'Portfolio', 'Contact us'];
+
         return React.createElement(
             'div',
             null,
-            menus.map((v, i) => {
+            this.state.menus.map((v, i) => {
                 return React.createElement(
                     'div',
                     { key: i },
